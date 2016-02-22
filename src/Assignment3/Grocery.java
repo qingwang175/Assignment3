d79d4ac5768e4f4c9f4992791b11fbff1ef374d5
sumshy2 <sumedhshah1994@yahoo.com>
2016-02-21 19:22:30 -0600

    Grocery class

--- src/Assignment3/Grocery.java
+++ src/Assignment3/Grocery.java
@@ -3,7 +3,29 @@ package Assignment3;
 public class Grocery extends Item {
 	//variables, constructor here
 	
+	public Grocery(String name, float price, int weight, int quantity, String premium){  
+		//for premium, user will input "P" or "NP" for PERISHABLE or NON-PERISHABLE
+		super(name,price,weight,quantity,premium);
+	}
 	//override calculatePrice() if necessary; Implement print methods as necessary	
 	// Only re-implement stuff you cannot get from the superclass (Item)
+	float calculatePrice(){
+		float final_price = 0;
+		if(premium.equals("P")) {    //premium shipping is 20% more
+			final_price += 24*weight*quantity;
+		} else if (premium.equals("NP")){   //standard shipping
+			final_price += 20*weight*quantity;
+		} 
+		final_price += price*quantity;
+		return final_price;
+	}
 	
+	void printItemAttributes(){
+		System.out.println("Item type: Grocery");
+		System.out.println("Item name: " + name);
+		System.out.printf("Item price: $%.2f%n",price);
+		System.out.println("Item quantity: " + quantity);
+		System.out.println("Item weight: " +  weight + " lbs");
+		System.out.println("Perishable (P or NP): " + premium);	
+	}
 }
